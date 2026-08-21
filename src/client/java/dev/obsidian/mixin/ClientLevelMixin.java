5ba0dc66157f0140ae394e389f27a450857aea11
TrissTheBoss <famnouwnow@gmail.com>
2026-08-21 16:03:54 +0200

    Fix exact Minecraft 26.2 ChunkPos accessors

--- src/client/java/dev/obsidian/mixin/ClientLevelMixin.java
+++ src/client/java/dev/obsidian/mixin/ClientLevelMixin.java
@@ -13,12 +13,12 @@ import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
 public abstract class ClientLevelMixin {
     @Inject(method = "onChunkLoaded", at = @At("TAIL"))
     private void obsidian$chunkLoaded(ChunkPos pos, CallbackInfo ci) {
-        SectionLifecycleEvents.chunkLoaded(pos.x, pos.z);
+        SectionLifecycleEvents.chunkLoaded(pos.x(), pos.z());
     }
 
     @Inject(method = "unload", at = @At("TAIL"))
     private void obsidian$chunkUnloaded(LevelChunk chunk, CallbackInfo ci) {
         ChunkPos pos = chunk.getPos();
-        SectionLifecycleEvents.chunkUnloaded(pos.x, pos.z);
+        SectionLifecycleEvents.chunkUnloaded(pos.x(), pos.z());
     }
 }
